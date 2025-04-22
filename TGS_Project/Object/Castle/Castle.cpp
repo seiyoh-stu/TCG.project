7d137150ac42b2aa160aab532e8ad1eace99d089
seiyoh-stu <138738958+seiyoh-stu@users.noreply.github.com>
2025-04-22 14:34:46 +0900

    castle追加

--- TGS_Project/Object/Castle/Castle.cpp
+++ TGS_Project/Object/Castle/Castle.cpp
@@ -1,1 +1,49 @@
 #include "Castle.h"
+#include "DxLib.h"
+
+Castle::Castle()
+{
+
+}
+
+Castle::~Castle()
+{
+
+}
+
+void Castle::Initialize()
+{
+    // 位置、当たり判定など初期値を設定（必要に応じて調整）
+    location = { 100, 100 };
+    z_layer = 1;
+    is_mobility = false;
+    filp_flag = false;
+    hit_flag = false;
+
+    // サイズや当たり判定なども必要に応じてここで設定する
+}
+
+void Castle::Draw(const Vector2D& screen_offset) const
+{
+    Vector2D draw_pos = location - screen_offset;
+
+    int width = 64;
+    int height = 64;
+    unsigned int color = GetColor(255, 0, 0); // 赤色
+
+    DrawBox(draw_pos.x, draw_pos.y, draw_pos.x + width, draw_pos.y + height, color, TRUE);
+}
+
+void Castle::Update(float delta_second) 
+{ 
+}
+
+void Castle::Finalize()
+{
+
+}
+
+//void OnHitCollision(GameBase* hit_object)
+//{
+//
+//}
--- TGS_Project/Object/Castle/Castle.h
+++ TGS_Project/Object/Castle/Castle.h
@@ -1,5 +1,16 @@
 #pragma once
-class Castle
+#include "../GameBase.h"
+class Castle : public GameBase
 {
+
+
+public:
+	Castle();
+	~Castle();
+    virtual void Initialize();
+    virtual void Update(float delta_second);
+    virtual void Draw(const Vector2D& screen_offset) const;
+    virtual void Finalize();
+    //virtual void OnHitCollision(GameBase* hit_object);
 };
 
