cd55ab1f558e3f5ef7e9a99c045d1e3113bcf0e8
kokubaryuki <138412750+kokubaryuki@users.noreply.github.com>
2025-05-01 09:48:24 +0900

    ヘルプ画面ゲームパット対応

--- TGS_Project/Scene/GameSub/Help/Help.cpp
+++ TGS_Project/Scene/GameSub/Help/Help.cpp
@@ -1,46 +1,43 @@
-﻿#include"Help.h"
-#include"../../../Utility/InputControl.h"
-#include"DxLib.h"
+﻿#include "Help.h"
+#include "../../../Utility/InputControl.h"
+#include "DxLib.h"
 
 Help::Help()
 {
-
 }
 
 Help::~Help()
 {
-
 }
 
 void Help::Initialize()
 {
-
 }
 
 eSceneType Help::Update(float delta_second)
 {
-	// 入力制御インスタンスの取得
-	InputControl* input = InputControl::GetInstance();
+    // 入力制御インスタンスの取得
+    InputControl* input = InputControl::GetInstance();
 
-	// SPACEキーが押されたら終了シーンへ遷移
-	if (input->GetKeyDown(KEY_INPUT_SPACE))
-	{
-		return eSceneType::eTitle;
-	}
+    // SPACEキーが押されたら終了シーンへ遷移
+    if (input->GetKeyDown(KEY_INPUT_SPACE) || input->GetPadButtonState(PAD_INPUT_2) == eInputState::ePress)
+    {
+        return eSceneType::eTitle;
+    }
 
-	return GetNowSceneType();
+    return GetNowSceneType();
 }
+
 void Help::Draw() const
 {
-	DrawFormatString(10, 10, GetColor(255, 255, 255), "ヘルプ画面です");
+    DrawFormatString(10, 10, GetColor(255, 255, 255), "ヘルプ画面です(Bで戻る)");
 }
 
 void Help::Finalize()
 {
-
 }
 
 eSceneType Help::GetNowSceneType() const
 {
-	return eSceneType::eHelp;
+    return eSceneType::eHelp;
 }
