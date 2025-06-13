8421631e4415382c50d66f57a91e1b92b8b25a10
kokubaryuki <138412750+kokubaryuki@users.noreply.github.com>
2025-06-13 16:35:19 +0900

    zombie4

--- TGS_Project/Object/Enemy/Enemy4.cpp
+++ TGS_Project/Object/Enemy/Enemy4.cpp
@@ -2,6 +2,7 @@
 #include"../../Object/GameObjectManager.h"
 #include"../../Utility/ScoreManager.h"
 #include"DxLib.h"
+#include "../../Utility/ResourceManager.h"
 
 #define MAX_HP 50
 
@@ -10,7 +11,7 @@ Enemy4::Enemy4() :
 	enemy4_y(580), // 初期位置Y座標
 	size4_x_(64),  // 四角の幅
 	size4_y_(64),  // 四角の高さ
-	color4_(GetColor(255, 255, 255)) // 四角の色 (赤)
+	zonbi4_walk_index(0), animation4_count(0)
 {
 
 }
@@ -26,6 +27,23 @@ void Enemy4::Initialize()
 	collision.box_size = 64;
 	collision.hit_object_type.push_back(eBullet);
 	collision.hit_object_type.push_back(eCastle);
+
+	ResourceManager* rm = ResourceManager::GetInstance();
+	std::vector<int> walk4_frames = rm->GetImages("Resource/Images/Enemy/Zombie_3/Walk.png", 10, 10, 1, 128, 128);
+
+
+
+	for (int i = 0; i < 10; ++i)
+	{
+		zonbi4_animation[i] = walk4_frames[i];
+	}
+
+	zonbi4_image = zonbi4_animation[0];  // 最初のアニメフレーム
+
+	location.x = enemy4_x;
+	location.y = enemy4_y;
+
+
 
 
 	hp = MAX_HP;
@@ -46,7 +64,7 @@ void Enemy4::Update(float delta_second)
 void Enemy4::Draw(const Vector2D& screen_offset) const
 {
 	//DrawBox(enemy_x, enemy_y, enemy_x + size_x_, enemy_y + size_y_, color_, TRUE);
-	DrawBox(location.x - collision.box_size.x, location.y - collision.box_size.y, location.x + collision.box_size.x, location.y + collision.box_size.y, color4_, TRUE);
+	DrawRotaGraph(location.x, location.y, 4.0f, 0.0f, zonbi4_image, TRUE);
 
 
 	Vector2D hp_bar = location;
@@ -112,7 +130,13 @@ void Enemy4::Movement()
 
 void Enemy4::AnimeControl()
 {
-	// アニメーションに関する処理を記述 (今回は空)
+	animation4_count++;
+	if (animation4_count >= 10)
+	{
+		animation4_count = 0;
+		zonbi4_walk_index = (zonbi4_walk_index + 1) % 10;
+		zonbi4_image = zonbi4_animation[zonbi4_walk_index];
+	}
 }
 
 //０６１２
